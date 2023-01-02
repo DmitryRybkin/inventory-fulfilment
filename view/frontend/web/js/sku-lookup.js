5a742844be6104ff327e72f6c2d8baf7f4b8c397
Dmitry Rybkin <rybka.dima1414@gmail.com>
2023-01-02 19:08:19 +0200

    IF-1 Adding from submit action via ajax, translate functions

--- view/frontend/web/js/sku-lookup.js
+++ view/frontend/web/js/sku-lookup.js
@@ -15,13 +15,26 @@ define([
 
     return Component.extend({
         defaults: {
-            sku: ko.observable("SKU"),
+            sku: ko.observable('24-MB01'),
             placeholder: $t('Example: %1').replace('%1', '24-MB01'),
+            messageResponse: ko.observable('')
         },
         initialize() {
             this._super();
 
             console.log('The skuLookup component has been loaded.');
         },
+        handleSubmit() {
+            this.messageResponse('');
+
+            storage.get(`rest/V1/products/${this.sku()}`)
+                .done(response => {
+                    console.log(response);
+                    this.messageResponse($t('Product found! %1').replace('%1', `<strong>${response.name}</strong>`));
+                })
+                .fail(() => {
+                    this.messageResponse($t('Product not found.'));
+                })
+        }
     });
 });
