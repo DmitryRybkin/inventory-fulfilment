6c00f59e28762f5ba4f666918c7b112ab1d2e255
Dmitry Rybkin <rybka.dima1414@gmail.com>
2023-01-04 16:16:43 +0200

    IF-1 Adding ko observable array with object, binding table

--- view/frontend/web/js/box-configurations.js
+++ view/frontend/web/js/box-configurations.js
@@ -1,15 +1,34 @@
 define([
-    'uiComponent'
+    'uiComponent',
+    'ko'
 ], function(
-    Component
+    Component,
+    ko
 ) {
     'use strict';
 
+    const boxConfiguration = () => {
+        return {
+            length: ko.observable(),
+            width: ko.observable(),
+            height: ko.observable(),
+            weight: ko.observable(),
+            unitsPerBox: ko.observable(),
+            numberOfBoxes: ko.observable(),
+        };
+    };
+
     return Component.extend({
+        defaults: {
+            boxConfigurations: ko.observableArray([boxConfiguration()])
+        },
         initialize() {
             this._super();
 
             console.log('The boxConfigurations component has been loaded.');
+        },
+        handleAdd() {
+            this.boxConfigurations.push(boxConfiguration());
         }
     });
 });
